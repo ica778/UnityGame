51bd2641e25551fb143c36e93db29c83a085bd61
ica778 <71621160+ica778@users.noreply.github.com>
2024-02-21 18:16:57 -0800

    own player character model is now invisible to own camera

--- Assets/Scripts/Player/PlayerVisual.cs
+++ Assets/Scripts/Player/PlayerVisual.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 
 public class PlayerVisual : NetworkBehaviour {
 
+    private MeshRenderer[] meshRenderers;
+
     private void Update() {
         if (!base.IsOwner) {
             return;
         }
 
+        MakePlayerCharacterInvisibleToOwnCamera();
+
         transform.rotation = PlayerCameraController.LocalInstance.GetCameraQuaternionOnlyYAxis();
+    }
+
+    private void MakePlayerCharacterInvisibleToOwnCamera() {
+        meshRenderers = gameObject.GetComponentsInChildren<MeshRenderer>();
+        foreach (MeshRenderer renderer in meshRenderers) {
+            renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+        }
     }
 }
