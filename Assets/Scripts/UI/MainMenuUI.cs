b63226e083e77556d6070d92be2f1519041b02d9
ica778 <71621160+ica778@users.noreply.github.com>
2024-04-15 00:53:49 -0700

    can host or join from menu scene

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -1,3 +1,5 @@
+using FishNet.Managing;
+using FishNet.Transporting;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,8 +9,23 @@ public class MainMenuUI : MonoBehaviour {
     [SerializeField] private Button hostButton;
     [SerializeField] private Button joinButton;
 
-    private void Awake() {
+    private NetworkManager networkManager;
+
+    private void Start() {
+        networkManager = FindObjectOfType<NetworkManager>();
+        if (networkManager == null) {
+            Debug.LogError("NetworkManager not found");
+            return;
+        }
+
         hostButton.onClick.AddListener(() => {
+            networkManager.ServerManager.StartConnection();
+            networkManager.ClientManager.StartConnection();
+            SceneLoader.Load(SceneLoader.Scene.GameScene);
+        });
+
+        joinButton.onClick.AddListener(() => {
+            networkManager.ClientManager.StartConnection();
             SceneLoader.Load(SceneLoader.Scene.GameScene);
         });
     }
