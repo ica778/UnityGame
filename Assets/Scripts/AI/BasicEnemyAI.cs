9c2c13524b42fb1ece37bb5f751f3ce1bb0388ba
ica778 <71621160+ica778@users.noreply.github.com>
2024-04-23 12:23:19 -0700

    rewrote vision detection ai to check multiple parts of target

--- Assets/Scripts/AI/BasicEnemyAI.cs
+++ Assets/Scripts/AI/BasicEnemyAI.cs
@@ -1,6 +1,7 @@
 using FishNet.Object;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -20,11 +21,19 @@ public class BasicEnemyAI : NetworkBehaviour {
     private float changePositionTimer = 0f;
     private NavMeshAgent navMeshAgent;
     private State currentState = State.Idle;
-    private Transform enemyTransform;
+    private Transform currentTransform;
+
+    private Vector3[] visionRaycastPositions = {
+        new Vector3(0, 0, 0),
+        new Vector3(0, 0.8f, 0),
+        new Vector3(0, -0.8f, 0),
+        new Vector3(0, 0.4f, 0),
+        new Vector3(0, -0.4f, 0),
+    };
 
     private void Awake() {
         navMeshAgent = GetComponent<NavMeshAgent>();
-        enemyTransform = transform;
+        currentTransform = transform;
     }
 
     private void Update() {
@@ -39,61 +48,79 @@ public class BasicEnemyAI : NetworkBehaviour {
 
     [ServerRpc(RequireOwnership = false)]
     private void EnemyBehaviour() {
-        Transform targetInOverlapSphereTransform = null;
-        bool targetDetected = false;
-
-        Collider[] collidersInRange = Physics.OverlapSphere(enemyTransform.position, detectionRadius, targetLayer);
-        // TODO: no consistent way of selecting a certain player if more than one in range
-        foreach (Collider collider in collidersInRange) {
-            // TODO: optimize by caching the transforms for each player collider
-            targetInOverlapSphereTransform = collider.transform;
-
-            targetDetected = CheckIfTargetDetectableByVision(targetInOverlapSphereTransform);
-
-            if (targetDetected) {
-                Debug.Log("TESTING TARGET DETECTED: " + targetInOverlapSphereTransform.position);
-                break;
-            }
-        }
+        Transform detectedTargetTransform = DetectTarget();
 
         switch (currentState) {
             case State.Idle:
-                if (targetDetected) {
+                if (detectedTargetTransform) {
                     currentState = State.Chasing;
                     Debug.Log("TESTING STATE CHANGED TO CHASING");
                 }
 
                 break;
             case State.Chasing:
-                if (!targetDetected) {
+                if (!detectedTargetTransform) {
                     currentState = State.Idle;
                     Debug.Log("TESTING STATE CHANGED TO IDLE");
                 }
                 else {
-                    //navMeshAgent.SetDestination(targetInRangeTransform.position);
+                    Debug.Log("TESTING HIT");
+                    //navMeshAgent.SetDestination(detectedTargetTransform.position);
                 }
 
                 break;
         }
     }
 
-    // TODO: make it check other parts of player by modifying the directionToTarget and targetAngle so that it raycasts other parts of the player. probably
-    // have to wait till player dimensions are finalized first though unless you want to make it really expensive and check a bunch of points. Have to make it
-    // check other points so if player's feet is sticking out or something the enemy can still see it.
-    private bool CheckIfTargetDetectableByVision(Transform targetInOverlapSphereTransform) {
-        Vector3 directionToTarget = (targetInOverlapSphereTransform.position - enemyTransform.position).normalized;
-        float targetAngle = Vector3.Angle(enemyTransform.forward, directionToTarget);
 
-        if (targetAngle < (viewAngle / 2)) {
-            float distanceToTarget = Vector3.Distance(enemyTransform.position, targetInOverlapSphereTransform.position);
 
-            if (distanceToTarget <= detectionRadius) {
-                if (!Physics.Raycast(enemyTransform.position, directionToTarget, distanceToTarget, obstacleLayer)) {
-                    return true;
-                }
-            }
+    private Vector3 GetDirectionToTarget(Vector3 targetPosition) {
+        return (targetPosition - currentTransform.position);
+    }
+
+    // TODO: check if this is more efficient than just raycasting to range of overlap sphere radius
+    private float GetDistanceToTarget(Vector3 targetPosition) {
+        return Vector3.Distance(currentTransform.position, targetPosition);
+    }
+
+    private bool CheckIfTargetIsInFieldOfVision(Vector3 directionToTarget) {
+        float angleToTarget = Vector3.Angle(currentTransform.forward, directionToTarget);
+        if (angleToTarget < (viewAngle / 2)) {
+            return true;
         }
         return false;
     }
 
+    private bool CheckIfTargetIsVisible(Vector3 targetPosition) {
+        Vector3 directionToTarget = GetDirectionToTarget(targetPosition);
+        if (!CheckIfTargetIsInFieldOfVision(directionToTarget)) {
+            return false;
+        }
+
+        float distanceToTarget = GetDistanceToTarget(targetPosition);
+        RaycastHit hitInfo;
+        bool hit = Physics.Raycast(currentTransform.position, directionToTarget, out hitInfo, distanceToTarget);
+        Debug.DrawRay(currentTransform.position, directionToTarget, Color.green, 1f);
+
+        if (hit && hitInfo.transform.gameObject.layer == LayerMask.NameToLayer("playerLayer")) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private Transform DetectTarget() {
+        Collider[] collidersInRange = Physics.OverlapSphere(currentTransform.position, detectionRadius, targetLayer);
+        foreach (Collider collider in collidersInRange) {
+            Transform currentTargetTransform = collider.transform;
+
+            foreach (Vector3 pos in visionRaycastPositions) {
+                if (CheckIfTargetIsVisible(currentTargetTransform.position + pos)) {
+                    return currentTargetTransform;
+                }
+            }
+            
+        }
+        return null;
+    }
 }
