dad9f00a86a9e1abb7e56fe452cd0a1f4cd64420
ica778 <71621160+ica778@users.noreply.github.com>
2024-04-28 02:24:13 -0700

    fixed kicking

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,7 +12,7 @@ public class GameManager : MonoBehaviour {
     }
 
     private void OnAskedToLeave() {
-        ConnectionManager.Instance.DisconnectFromServer();
+        QuitGame();
     }
 
     public void QuitGame() {
