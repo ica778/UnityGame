82d57752128f25d14c8c29547e8a6980df0529cb
ica778 <71621160+ica778@users.noreply.github.com>
2024-05-23 01:26:56 -0700

    test main menu work with additive scenes

--- Assets/Scripts/MainMenu/TestMainMenuManager.cs
+++ Assets/Scripts/MainMenu/TestMainMenuManager.cs
@@ -1,7 +1,10 @@
+using FishNet;
 using FishNet.Managing;
+using FishNet.Managing.Scened;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class TestMainMenuManager : MonoBehaviour {
@@ -9,6 +12,10 @@ public class TestMainMenuManager : MonoBehaviour {
     [SerializeField] private Button joinButton;
 
     private NetworkManager networkManager;
+
+    private void Awake() {
+        StartCoroutine(StartNetworkingScene());
+    }
 
     private void Start() {
         networkManager = FindObjectOfType<NetworkManager>();
@@ -18,14 +25,54 @@ public class TestMainMenuManager : MonoBehaviour {
         }
 
         hostButton.onClick.AddListener(() => {
-            networkManager.ServerManager.StartConnection();
-            networkManager.ClientManager.StartConnection();
-            SceneHandler.Load(SceneHandler.Scenes.GameScene);
+
+            StartCoroutine(StartHost());
         });
 
         joinButton.onClick.AddListener(() => {
-            networkManager.ClientManager.StartConnection();
-            SceneHandler.Load(SceneHandler.Scenes.GameScene);
+            
+            StartCoroutine(StartGameScenes());
         });
+    }
+
+    // NOTE: maybe duplicate code
+    private IEnumerator StartHost() {
+            networkManager.ServerManager.StartConnection();
+            networkManager.ClientManager.StartConnection();
+        AsyncOperation asyncOperation = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("GameScene", LoadSceneMode.Additive);
+
+        while (!asyncOperation.isDone) {
+            yield return null;
+        }
+
+        UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync("TestMainMenu");
+
+        SceneLoadData sld = new SceneLoadData("GameScene");
+        InstanceFinder.SceneManager.LoadGlobalScenes(sld);
+
+        UnityEngine.SceneManagement.SceneManager.SetActiveScene(UnityEngine.SceneManagement.SceneManager.GetSceneByName("GameScene"));
+    }
+
+    // NOTE: maybe duplicate code
+    private IEnumerator StartGameScenes() {
+        networkManager.ClientManager.StartConnection();
+        UnityEngine.AsyncOperation asyncOperation = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("GameScene", LoadSceneMode.Additive);
+
+        while (!asyncOperation.isDone) {
+            yield return null;
+        }
+
+        UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync("TestMainMenu");
+
+        UnityEngine.SceneManagement.SceneManager.SetActiveScene(UnityEngine.SceneManagement.SceneManager.GetSceneByName("GameScene"));
+    }
+
+    // NOTE: maybe duplicate code
+    private IEnumerator StartNetworkingScene() {
+        AsyncOperation asyncOperation = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("NetworkingSceneTest", LoadSceneMode.Additive);
+
+        while (!asyncOperation.isDone) {
+            yield return null;
+        }
     }
 }
