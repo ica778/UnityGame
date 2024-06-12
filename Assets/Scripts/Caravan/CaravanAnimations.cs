c0f8e3d3e1d271f41712ada762301a1e4a068d2d
ica778 <71621160+ica778@users.noreply.github.com>
2024-06-11 23:32:40 -0700

    synced caravan

--- Assets/Scripts/Caravan/CaravanAnimations.cs
+++ Assets/Scripts/Caravan/CaravanAnimations.cs
@@ -1,9 +1,10 @@
 using FishNet;
+using FishNet.Object;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CaravanAnimations : MonoBehaviour {
+public class CaravanAnimations : NetworkBehaviour {
     [SerializeField] private Animator animator;
 
     // Animations
@@ -14,7 +15,10 @@ public class CaravanAnimations : MonoBehaviour {
     }
 
     private void SceneManager_OnLoadEnd(FishNet.Managing.Scened.SceneLoadEndEventArgs obj) {
-        ResumeAnimation();
+        if (base.IsServerInitialized) {
+            ResumeAnimationServerRpc();
+        }
+        
         // NOTE: probably need to implement this commented-out code to verify scenes fully loaded if you choose to load more than 1 scene per level
         /*
         UnityEngine.SceneManagement.Scene gameScene = UnityEngine.SceneManagement.SceneManager.GetSceneByName(SceneName.GameScene.ToString());
@@ -28,13 +32,37 @@ public class CaravanAnimations : MonoBehaviour {
         */
     }
 
+    [ServerRpc(RequireOwnership = false)]
+    private void ResumeAnimationServerRpc() {
+        ResumeAnimationObserversRpc();
+    }
+
+    [ObserversRpc]
+    private void ResumeAnimationObserversRpc() {
+        ResumeAnimation();
+    }
+
     public void MoveCaravan() {
+        MoveCaravanServerRpc();
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void MoveCaravanServerRpc() {
+        MoveCaravanObserversRpc();
+    }
+
+    [ObserversRpc]
+    private void MoveCaravanObserversRpc() {
         animator.SetTrigger(TRIGGER_CARAVAN_MOVE);
     }
 
     public void PauseAnimation() {
         animator.speed = 0f;
-        GameSceneManager.Instance.LoadCaravanLeverPulledScenes();
+        if (base.IsHostInitialized) {
+            Debug.Log("TESTING CHANGED SCENE");
+            GameSceneManager.Instance.LoadCaravanLeverPulledScenes();
+        }
+        
     }
 
     public void ResumeAnimation() {
