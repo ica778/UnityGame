0f92a1edca96aac1c7821d7f4ae9bf5d3a82f157
ica778 <71621160+ica778@users.noreply.github.com>
2024-06-17 23:27:46 -0700

    caravan objects parenting functionality added

---
+++ Assets/Scripts/Caravan/CaravanObjectsHandler.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaravanObjectsHandler : MonoBehaviour {
+    [SerializeField] private BoxCollider interiorCollider;
+    [SerializeField] private Transform caravanContentsParent;
+
+    // TODO: RESEARCH REBUILDOBSERVERS() FISHNET FUNCTION WHEN MOVING OBJECTS TO DIFFERENT SCENEct 
+    private void OnTriggerEnter(Collider other) {
+        Transform obj = GetTopmostParent(other.transform);
+        obj.SetParent(caravanContentsParent);
+    }
+
+    private void OnTriggerExit(Collider other) {
+        Transform obj = GetTopmostParent(other.transform);
+        obj.SetParent(null);
+        UnityEngine.SceneManagement.SceneManager.MoveGameObjectToScene(obj.gameObject, UnityEngine.SceneManagement.SceneManager.GetSceneByName(SceneName.GamePersistentObjectsScene.ToString()));
+    }
+
+    private Transform GetTopmostParent(Transform obj) {
+        Transform current = obj;
+        while (current.parent != null && current.parent != caravanContentsParent) {
+            current = current.parent;
+        }
+        return current;
+    }
+}
