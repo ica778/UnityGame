eb4ebb15fb047458656a4a36fa4729368285bcec
ica778 <71621160+ica778@users.noreply.github.com>
2024-06-28 23:11:37 -0700

    synced damagereceiver and also added editor script for scene on play button

--- Assets/Scripts/Combat/DamageReceiver.cs
+++ Assets/Scripts/Combat/DamageReceiver.cs
@@ -1,11 +1,27 @@
+using FishNet.Connection;
+using FishNet.Object;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 // NOTE: PUT THIS SCRIPT ON THE HITBOXES PARENT. SO THE GAME OBJECT THIS SCRIPT IS ON SHOULD BE PARENT OF COLLIDERS WHICH ARE HITBOXES.
-public class DamageReceiver : MonoBehaviour {
+public class DamageReceiver : NetworkBehaviour {
 
-    public void ReceiveHit(float damage) {
+    public void ReceiveHit(int damage) {
         Debug.Log("TESTING RECEIVING HIT FOR DAMAGE: " + damage);
+        ReceiveHitServerRpc(damage, base.LocalConnection);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void ReceiveHitServerRpc(int damage, NetworkConnection conn) {
+        ReceiveHitObserversRpc(damage, conn);
+    }
+
+    [ObserversRpc(ExcludeOwner = true)]
+    private void ReceiveHitObserversRpc(int damage, NetworkConnection conn) {
+        if (base.LocalConnection == conn) {
+            return;
+        }
+        Debug.Log("TESTING RECEIVING HIT BY ANOTHER PLAYER FOR DAMAGE: " + damage);
     }
 }
