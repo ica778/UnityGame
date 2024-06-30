e48639eaa1ca24a80d3d6a3f84f41f2a29c687a5
ica778 <71621160+ica778@users.noreply.github.com>
2024-06-30 16:52:40 -0700

    fixed error and added host finished loading event

--- Assets/Scripts/SceneManagement/LoadingScreen.cs
+++ Assets/Scripts/SceneManagement/LoadingScreen.cs
@@ -8,11 +8,13 @@ public class LoadingScreen : NetworkBehaviour {
     public override void OnStartNetwork() {
         SceneLoading.Instance.OnFinishedLoadingStartScenes += SceneLoading_OnFinishedLoadingStartScenes;
         SceneLoading.Instance.OnFinishedLoadingLevelScenes += SceneLoading_OnFinishedLoadingLevelScenes;
+        SceneLoading.Instance.OnHostFinishedLoadingStartScenes += SceneLoading_OnHostFinishedLoadingStartScenes;
     }
 
     public override void OnStopNetwork() {
         SceneLoading.Instance.OnFinishedLoadingStartScenes -= SceneLoading_OnFinishedLoadingStartScenes;
         SceneLoading.Instance.OnFinishedLoadingLevelScenes -= SceneLoading_OnFinishedLoadingLevelScenes;
+        SceneLoading.Instance.OnHostFinishedLoadingStartScenes -= SceneLoading_OnHostFinishedLoadingStartScenes;
     }
 
     private void SceneLoading_OnFinishedLoadingStartScenes(object sender, System.EventArgs e) {
@@ -21,5 +23,9 @@ public class LoadingScreen : NetworkBehaviour {
 
     private void SceneLoading_OnFinishedLoadingLevelScenes(object sender, System.EventArgs e) {
         Debug.Log("THIS CLIENT HAS FINISHED LOADING LEVEL SCENES");
+    }
+
+    private void SceneLoading_OnHostFinishedLoadingStartScenes(object sender, System.EventArgs e) {
+        Debug.Log("HOST HAS FINISHED LOADING INITIAL SCENES");
     }
 }
